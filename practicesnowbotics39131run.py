39df9ee69337da88803984686849756f7ae2ced1
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2026-09-04 01:40:14 +0000

    Add files via upload
    crosses the map. start on left base on on square closest to the right.

---
+++ practicesnowbotics39131run.py
@@ -0,0 +1,7 @@
+from QuadBotPortMap import *
+drivebase.straight(300)
+drivebase.turn(90)
+drivebase.settings(straight_speed=500)
+drivebase.straight(1300)
+drivebase.turn(45)
+drivebase.straight(200)
