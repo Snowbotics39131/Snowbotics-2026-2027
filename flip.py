9cef5b7f897190bb8c27eb6f752fb70cda7cd541
Uxugin <84234447+UxuginPython@users.noreply.github.com>
2026-09-13 15:35:31 -0600

    add flippy code

---
+++ flip.py
@@ -0,0 +1,8 @@
+#3 from red line; on black line
+from QuadBotPortMap import *
+drivebase.straight(500)
+drivebase.turn(90)
+drivebase.straight(-150)
+motorLeft.dc(-100)
+motorRight.dc(100)
+wait(2000)
