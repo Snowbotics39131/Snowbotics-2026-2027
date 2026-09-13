3e99e7e7d72d8cfe862cb1e928f0b11223564a80
Uxugin <84234447+UxuginPython@users.noreply.github.com>
2026-09-13 17:18:18 -0600

    all today's changes from Collin, Aaron, and Krish

--- flip.py
+++ flip.py
@@ -2,7 +2,7 @@
 from QuadBotPortMap import *
 drivebase.straight(500)
 drivebase.turn(90)
-drivebase.straight(-150)
+drivebase.straight(-80)
 motorLeft.dc(-100)
 motorRight.dc(100)
-wait(2000)
+wait(8000)
--- gardenskylight.py
+++ gardenskylight.py
@@ -2,9 +2,15 @@
 #2nd thick line from the left (8th square)
 #1 forward from back line
 from QuadBotPortMap import *
-drivebase.straight(560)
+drivebase.straight(561)
 drivebase.turn(90)
 drivebase.straight(615)
 drivebase.straight(-60)
 drivebase.turn(90)
-drivebase.straight(200)
+drivebase.straight(250)
+drivebase.turn(-90)
+drivebase.straight(340)
+drivebase.turn(20)
+drivebase.straight(-90)
+drivebase.straight(700)
+drivebase.turn(-20)
