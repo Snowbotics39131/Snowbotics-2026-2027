90603b3fdfa34a5558141d451a961ad90ceb7680
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2026-09-18 00:55:44 +0000

    updated port map for current 2026  robot .Audrey Thomas

---
+++ PortMap2026.py
@@ -0,0 +1,21 @@
+from pybricks.hubs import InventorHub
+from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import *
+
+hub = InventorHub()
+
+motorLeft = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+motorRight = Motor(Port.C, Direction.CLOCKWISE)
+moterback = Motor(Port.D, Direction.CLOCKWISE)
+moterfront = Motor(Port.F, Direction.CLOCKWISE)
+colorSensorLeft = ColorSensor(Port.A)
+colorSensorRight = ColorSensor(Port.E)
+drivebase = DriveBase(motorLeft,motorRight,56,96.5)
+
+drivebase.use_gyro(True)
+
+print(hub.battery.voltage())
+#print(motorAttachment.control.pid())
+#print(motorAttachment.control.target_tolerances())
