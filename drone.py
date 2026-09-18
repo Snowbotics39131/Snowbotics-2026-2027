2289ff14a602ac5223a5486f1d73db266a94a6a6
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2026-09-18 01:20:27 +0000

    compleats the drone mission with new port map

--- drone.py
+++ drone.py
@@ -1,7 +1,9 @@
 #2 from red line
-#0.5 from black line
-from QuadBotPortMap import *
+# A third from black line
+# facing right
+from PortMap2026 import *
 drivebase.settings(straight_speed=300)
 #drivebase.straight(605)
-drivebase.arc(-1800, distance=650)
-drivebase.arc(-1500, distance=-600)
+drivebase.arc(-1650, distance=650)
+drivebase.turn(25)
+drivebase.straight(-650)
