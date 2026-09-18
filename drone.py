2e6227cc003d6abee5e971fd361cd2d28d6a3da5
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2026-09-18 01:53:40 +0000

    changed the distence of the circle fro the drone mission .audrey thomas

--- drone.py
+++ drone.py
@@ -1,9 +1,8 @@
 #2 from red line
-# at black line
+#from black line
 # facing right
 from PortMap2026 import *
 drivebase.settings(straight_speed=300)
-#drivebase.straight(605)
-drivebase.arc(-1650, distance=650)
+drivebase.arc(-1650, distance=652)
 drivebase.turn(25)
 drivebase.straight(-650)
