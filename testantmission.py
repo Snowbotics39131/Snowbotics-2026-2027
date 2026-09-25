bd67e40aa143a5e10c7ae66bd87a65e5b567a0a9
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2026-09-25 02:30:12 +0000

    this is lined up at the ant and compleatesthe white thing and the root door thing .Audrey Thomas

---
+++ testantmission.py
@@ -0,0 +1,11 @@
+from PortMapUltimate import *
+#right part of the moter is lined up with the ant and backwards
+# TODO:turn and line up with the tree growing mission and drop the heavy block
+drivebase.settings(straight_speed=150)
+drivebase.straight(40)
+drivebase.turn(-90)
+drivebase.turn(15)
+drivebase.straight(250)
+drivebase.straight(-50)
+drivebase.turn(-45)
+drivebase.straight(90)
